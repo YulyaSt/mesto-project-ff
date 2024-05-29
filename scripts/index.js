e475ad71a8ce66729b421bd034742a2d8f05682d
Yulia Stromenko <yulya.stromenko@yandex.ru>
2024-05-30 00:42:33 +0300

    Mesto_project-5_review

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
-const placesList = document.querySelector('.places__list');
-const cardTemplate = document.querySelector('#card-template').content; 
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 
-function addElement(cardValue, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  const nameValue= cardValue.name;
-  const linkValue = cardValue.link;
+  const nameValue= cardData.name;
+  const linkValue = cardData.link;
 
   cardElement.querySelector('.card__title').textContent = nameValue;
   cardElement.querySelector('.card__image').src = linkValue;
+  cardElement.querySelector('.card__image').alt = nameValue;
 
-  deleteButton.addEventListener('click', deleteCard);
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
 
   return cardElement;
 };
 
 
-function deleteCard(evt) {
-  const delCard = evt.target.closest('.card');
-  delCard.remove();
+function deleteCard(cardElement) {
+  cardElement.remove();
 };
 
 
-initialCards.forEach(cardValue => {
-  placesList.append(
-    addElement(cardValue, deleteCard)
+initialCards.forEach(cardData => {
+  cardsContainer.append(
+    createCard(cardData, deleteCard)
   );
 });
